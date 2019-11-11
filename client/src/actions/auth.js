cf82e08d236fa6f3e2ef9d5e12c0b9914977717d
Damian Ly <dsly@uci.edu>
2019-11-10 22:29:17 -0800

    Added dashboard logic to detect whether a user has a profile or not and display corresponding components

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,7 +8,8 @@ import {
 	AUTH_ERROR,
 	LOGIN_SUCCESS,
 	LOGIN_FAIL,
-	LOGOUT
+	LOGOUT,
+	CLEAR_PROFILE
 } from './types';
 
 // Load user
@@ -100,5 +101,6 @@ export const register = ({ name, email, password }) => async dispatch => {
 
 // Logout / Clear Profile
 export const logout = () => dispatch => {
+	dispatch({ type: CLEAR_PROFILE })
 	dispatch({ type: LOGOUT })
 }
--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -2,11 +2,14 @@ import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
 
 export default () => {
-	<Fragment>
-		<imsg>
-			src={spinner}
-			style={{ width: '200px', margin: 'auto', display: 'block' }}
-			alt='loading...';
-		</img>
-	</Fragment>
+	const imgStyle = { width: '200px', margin: 'auto', display: 'block' };
+	return (
+		<Fragment>
+			<img
+				src={spinner}
+				style={{ width: '200px', margin: 'auto', display: 'block' }}
+				alt='loading...'
+			/>
+		</Fragment>
+		);
 }
