209435c055efa40a65c7a1baecf9f865ee6a0311
damian_ly <dly@leasecalcs.com>
2020-04-24 16:35:23 -0700

    working on adding the experience and aducation functionality

---
+++ client/src/components/profile-forms/AddEducation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+import { addExperience } from '../../actions'
+
+const AddExperience = props => {
+    return (
+        <div>
+        </div>
+    )
+}
+
+AddExperience.propTypes = {
+
+}
+
+export default AddExperience;
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,6 +1,8 @@
 import {
 	GET_PROFILE,
 	PROFILE_ERROR,
+	CLEAR_PROFILE,
+	UPDATE_PROFILE,
 } from '../actions/types';
 
 const initialState = {
@@ -16,6 +18,7 @@ export default function(state = initialState, action) {
 
 	switch(type){
 		case GET_PROFILE:
+		case UPDATE_PROFILE:
 			return {
 				...state,
 				profile: payload,
@@ -25,6 +28,13 @@ export default function(state = initialState, action) {
 			return {
 				...state,
 				error: payload,
+				loading: false,
+			}
+		case CLEAR_PROFILE:
+			return {
+				...state,
+				profile: null,
+				repos: [],
 				loading: false,
 			}
 		default:
