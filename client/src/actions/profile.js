5118767ebd2282c4692587028f92e13bc7e3d37c
damian_ly <dly@leasecalcs.com>
2020-05-04 14:09:52 -0700

    added skeleton post functionality, setup reducers and action files

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -66,7 +66,7 @@ export const getGithubRepos = username => async dispatch => {
 // Get profile by ID
 export const getProfileById = userId => async dispatch => {
 	try{
-		const res = await axios.get(`/api/profile/${userId}`);
+		const res = await axios.get(`/api/profile/user/${userId}`);
 
 		dispatch({
 			type: GET_PROFILE,
